e2deb078c91404a1148e7615c650660dce91a383
rschmidt14 <scape.list.moderator@gmail.com>
2026-01-13 20:02:10 +0100

    added lehrer db mit kv

---
+++ lehrer-db-mit-kv.sql
@@ -0,0 +1,39 @@
+drop table if exists lv cascade;
+drop table if exists s cascade;
+drop table if exists l cascade;
+
+create table LV (
+  id int,
+  fach char,
+  stunden int,
+  jahr int
+);
+
+create table L (
+  id int,
+  name text,
+  ALTER int,
+  pendler bool  
+);
+
+create table S (
+  sid int,
+  name text,
+  alter int,
+  pendler bool,
+  kv int
+);
+
+truncate l;
+insert into l values
+(1, 'Max', 25, true),
+(2, 'Fritz', 31, false),
+(3, 'Gabi', 31, true),
+(4, 'Moritz', 33, false);
+
+insert into lv values 
+(1, 'D', 4, 2022),
+(1, 'E', 4, 2022),
+(2, 'E', 6, 2022),
+(3, 'D', 2, 2022);
+
